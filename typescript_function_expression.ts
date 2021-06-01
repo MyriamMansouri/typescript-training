ab43e798e991cf501a469dc470287126e67672d0
Myriam Mansouri <myriam.mansouri@radio-canada.ca>
2021-06-01 12:25:19 -0400

    built examples for TS function expressions

---
+++ typescript_function_expression.ts
@@ -0,0 +1,23 @@
+
+// function declaration
+const fullName = (first: string, last: string): string => {
+    return first + ' ' + last;
+}
+
+// function expression
+let otherFullName : (first: string, last: string) => string;
+otherFullName = (first, last) => {
+    return first + ' ' + last;
+}
+
+let thirdFullName: (first: string, last: string) => string = (first: string, last: string): string => {
+    return first + ' ' + last;
+}
+
+console.log(fullName('Lady', 'Gaga'))
+console.log(otherFullName('Lady', 'Gaga'))
+console.log(thirdFullName('Lady', 'Gaga'))
+
+// declaring a function like what we dit for ==> otherFullName <== (ie with function expression before declaration)
+// prevents hoisting
+// which means we cannot invoke the function at the top of our code, before the declaration
